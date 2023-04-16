cb9b6397b99dd80d5d7f1869860a09917aa6e82d
abhirupacharya <abhirup.acharya009@gmail.com>
2023-04-16 21:23:51 +0530

    initial base setup

---
+++ client/src/App.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import { Route, Routes } from 'react-router-dom';
+
+
+const App = () => {
+  return (
+    <div className="relative sm:-8 p-4 bg-[#13131a] min-h-screen flex flex-row">
+      <div className="sm:flex hidden mr-10 relative">
+        App
+      </div>
+    </div>
+  )
+}
+
+export default App
