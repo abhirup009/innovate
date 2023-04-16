67b8b074665c91142a1f23b76b7ec1c6db156eb6
Abhirup Acharya <abhirup.acharya009@gmail.com>
2023-04-17 02:35:39 +0530

    Add sidebar components (#1)

--- client/src/components/Sidebar.jsx
+++ client/src/components/Sidebar.jsx
@@ -21,18 +21,18 @@ const Sidebar = () => {
   return (
     <div className="flex justify-between items-center flex-col sticky top-5 h-[93vh]">
       <Link to="/">
-        <Icon styles="w-[52px] h-[52px] bg-[#2c2f32]" imgUrl={logo} />
+        <Icon styles="w-[52px] h-[52px] bg-[#1c1c24]" imgUrl={logo} />
       </Link>
 
       <div className="flex-1 flex flex-col justify-between items-center bg-[#1c1c24] rounded-[20px] w-[76px] py-4 mt-12">
-        <div className="flex flex-col justify-center items-center gap-3">
+        <div className="flex flex-col justify-center items-center gap-5">
           {navlinks.map((link) => (
-            <Icon 
+            <Icon
               key={link.name}
               {...link}
               isActive={isActive}
               handleClick={() => {
-                if(!link.disabled) {
+                if (!link.disabled) {
                   setIsActive(link.name);
                   navigate(link.link);
                 }
@@ -40,8 +40,6 @@ const Sidebar = () => {
             />
           ))}
         </div>
-
-        <Icon styles="bg-[#1c1c24] shadow-secondary" imgUrl={sun} />
       </div>
     </div>
   )
