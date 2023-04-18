088fa4121675a9db770eed076216498549c708fb
Abhirup Acharya <abhirup.acharya009@gmail.com>
2023-04-19 01:57:50 +0530

    Add navbar setup (#3)
    * Add navbar setup
    
    * Minor refactors

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -11,6 +11,11 @@ const App = () => {
         <Sidebar />
       </div>
 
+      <div className="flex-1 max-sm:w-full max-w-[1280px] mx-auto sm:pr-5">
+        <Navbar />
+
+      </div>
+
     </div>
   )
 }
